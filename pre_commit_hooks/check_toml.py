cafb8eb265a25e0cc8cd03302d9c5b280545b2af
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-05-24 08:44:13 +0200

    Use tomli and tomllib instead of toml

--- pre_commit_hooks/check_toml.py
+++ pre_commit_hooks/check_toml.py
@@ -1,25 +1,30 @@
 from __future__ import annotations
 
 import argparse
+import sys
 from typing import Sequence
 
-import toml
+if sys.version_info >= (3, 11):
+    import tomllib
+else:
+    import tomli as tomllib
 
 
 def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument('filenames', nargs='*', help='Filenames to check.')
+    parser.add_argument("filenames", nargs="*", help="Filenames to check.")
     args = parser.parse_args(argv)
 
     retval = 0
     for filename in args.filenames:
         try:
-            toml.load(filename)
-        except toml.TomlDecodeError as exc:
-            print(f'{filename}: {exc}')
+            with open(filename, mode="rb") as fp:
+                tomllib.load(fp)
+        except tomllib.TOMLDecodeError as exc:
+            print(f"{filename}: {exc}")
             retval = 1
     return retval
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     raise SystemExit(main())
